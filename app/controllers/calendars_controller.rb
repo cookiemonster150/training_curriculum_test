94f2c3c833e07e153af369de775c77ee10bb03cc
cookiemonster150 <160557231+cookiemonster150@users.noreply.github.com>
2024-03-05 12:20:26 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -26,6 +25,17 @@ class CalendarsController < ApplicationController
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
+     # 今日の曜日の数値を取得
+    today_wday_num = @todays_date.wday
+
+    # wdaysから今日の曜日の文字列を取り出す
+    today_wday = wdays[today_wday_num]
+
+    # 今日の曜日以外の曜日の情報を取得
+    other_wday_index = 3 # 例えば、水曜日を取り出す場合
+
+    other_wday = wdays[other_wday_index]
+
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
@@ -35,9 +45,24 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      # wdayメソッドを用いて取得した数値
+      wday_num = (@todays_date + x).wday
+
+      # 「wday_numが7以上の場合」の条件式
+      if wday_num >=7
+        wday_num = wday_num - 7
+      end
+
+      # wdaysから値を取り出す記述
+      wday_value = wdays[wday_num]
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans,  :wday => wday_value }
       @week_days.push(days)
     end
+
+    # 今日以外の曜日の情報を取り出す
+    other_wday
 
   end
 end
